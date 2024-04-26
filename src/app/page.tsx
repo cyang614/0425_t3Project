9728e968098ec04d837a550027086598f56e7787
cyang614 <cyang5075@gmail.com>
2024-04-26 22:28:09 +0800

    0426

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
       <div className="flex flex-wrap gap-4">{
         mockImages.map((image)=>(
           <div key={image.id} className="w-48">
-            <img src="{image.url}" />
+            <img src={image.url} />
           </div>
         ))
       }</div>
