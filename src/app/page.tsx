d942764e072a7e575754d92c3b9ec0b56c230337
cyang614 <cyang5075@gmail.com>
2024-04-28 14:30:46 +0800

    0428

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,20 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
 
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany(
+    {orderBy:(model,{asc})=>asc(model.id)},
+  );
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {
         [...images,...images,...images].map((image,index)=>(
-          <div key={image.id + "-" + index} className="w-48">
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
+            <div>
+              {image.name}
+            </div>
           </div>
         ))
       }</div>
