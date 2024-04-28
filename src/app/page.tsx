a49af742f3499388f3155624c3dd73bd1c6b6d09
cyang614 <cyang5075@gmail.com>
2024-04-28 11:09:00 +0800

    0428

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
  * pnpm run db:studio
 */
 
+
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls= [
   "https://utfs.io/f/189c18ca-8a4a-4645-9f83-fa83d2ce80f3-9hjed0.jpg",
@@ -17,12 +19,22 @@ const mockImages = mockUrls.map((url,index)=>({
 }))
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        [...mockImages,...mockImages,...mockImages].map((image)=>(
-          <div key={image.id} className="w-48">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post)=>(
+          <div key={post.id}>
+            {post.name}
+          </div>
+        ))}
+        {
+        [...mockImages,...mockImages,...mockImages].map((image,index)=>(
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))
