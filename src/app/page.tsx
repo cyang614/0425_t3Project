9bfeaa983359534640cf5c2383b0db76f4849e71
cyang614 <cyang5075@gmail.com>
2024-05-03 23:33:09 +0800

    0503

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-/**https://www.youtube.com/watch?v=d5x0JCZbAJs&t 1:22:59 
+/**https://www.youtube.com/watch?v=d5x0JCZbAJs&t 1:37:30 
  * pnpm run db:studio
  * pnpm dev
  * use vercel . Clerk . uploadthing
@@ -6,6 +6,7 @@
 
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -19,12 +20,14 @@ async function Images(){
     {
     images.map((image)=>(
       <div key={image.id} className="flex h-48 w-48 flex-col">
+        <Link href={`/img/${image.id}`}>
         <Image 
         src={image.url} 
         style={{objectFit:"contain"}}
         width={192}
         height={192} 
         alt={image.name}/>
+        </Link>
         <div>
           {image.name}
         </div>
