1aab203ae9380463508e0dab68bd28d32af9d2eb
cyang614 <cyang5075@gmail.com>
2024-05-04 16:27:40 +0800

    0504

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,7 +1,9 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props:{id:number}){
     const image = await getImage(props.id);
+    const uploaderInfo = await clerkClient.users.getUser(image.userId);
     return (
             <div className="flex h-full w-full min-w-0">
                 <div className="flex-shrink flex items-center justify-center">
@@ -9,7 +11,15 @@ export default async function FullPageImageView(props:{id:number}){
                 </div>
             
                 <div className="flex w-48 flex-col flex-shrink-0 border-l">
-                    <div className="text-xl font-bold">{image.name}</div>
+                    <div className="border-b p-2 text-center text-lg">{image.name}</div>
+                    <div className="flex flex-col p-2">
+                        <span>創建者:</span>
+                        <span>{uploaderInfo.fullName}</span>
+                    </div>
+                    <div className="flex flex-col p-2">
+                        <span>上傳日期:</span>
+                        <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+                    </div>
                 </div>
             </div>
     )
